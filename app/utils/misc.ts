b8718055f40d2f3e00f383bcf5a226a7a2d06fa9
waptik <itswaptik@gmail.com>
2024-05-03 01:49:30 +0000

    chore: helper utils to extract username from fc url

--- app/utils/misc.ts
+++ app/utils/misc.ts
@@ -6,3 +6,13 @@ export function getEnvOrThrow(name: string) {
 
   return value;
 }
+
+// create a sleep function to delay the sending of messages
+export const sleep = (ms: number) =>
+  new Promise((resolve) => setTimeout(resolve, ms));
+
+export function extractWarpcastHandle(url: string) {
+  if (!url) return null;
+  const match = url.match(/^https?:\/\/(www\.)?warpcast.com\/@?(?<handle>\w+)/);
+  return match?.groups?.handle ? match.groups.handle.toLowerCase() : null;
+}
