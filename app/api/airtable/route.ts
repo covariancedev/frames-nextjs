03054afb40b901574a52d9df0a8a8850aafe6044
waptik <itswaptik@gmail.com>
2024-05-05 12:27:44 +0000

    working on airtable api route

--- app/api/airtable/route.ts
+++ app/api/airtable/route.ts
@@ -5,3 +5,6 @@ export async function GET() {
   const hubs = await getHubs();
   return Response.json({ hubs, airtableData });
 }
+
+export const dynamic = "force-dynamic"; // defaults to auto
+export const revalidate = 60;
--- app/utils/airtable/utils.ts
+++ app/utils/airtable/utils.ts
@@ -2,13 +2,18 @@ import { airtable } from "./client";
 
 export async function getHubs() {
   const hubs: Record<string, string>[] = [];
-  const res = await airtable.hubs.select().all();
 
-  for (const rec of res) {
-    hubs.push({
-      id: rec.id,
-      name: rec.fields["Hub Name"] as string,
-    });
+  try {
+    const res = await airtable.hubs.select().all();
+
+    for (const rec of res) {
+      hubs.push({
+        id: rec.id,
+        name: rec.fields["Hub Name"] as string,
+      });
+    }
+  } catch (e) {
+    console.error("Error fetching hubs", e);
   }
   return hubs;
 }
