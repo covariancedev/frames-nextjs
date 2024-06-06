bce8ab54c35ae9e0e6553cb11fb468efffc8c8e5
TheVirginBrokey <itswaptik@gmail.com>
2024-06-06 16:54:12 +0000

    install and setup next-axiom

--- next.config.mjs
+++ next.config.mjs
@@ -1,31 +1,38 @@
+import { withAxiom } from "next-axiom";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          hostname: "*",
-          protocol: "http",
-        },
-        {
-          hostname: "*",
-          protocol: "https",
-        },
-      ],
-    },
-    experimental: {
-      serverComponentsExternalPackages: ["@resvg/resvg-js", "airtable","@olli/kvdex","@deno/kv"],
-    },
-    webpack: (config) => {
-      config.externals.push({
-        sharp: "commonjs sharp",
-        "@resvg/resvg-js": "commonjs @resvg/resvg-js",
-      });
-  
-      return config;
-    },
-    typescript:{
-      ignoreBuildErrors: true,
-    }
-  };
-  
-  export default nextConfig;
+  images: {
+    remotePatterns: [
+      {
+        hostname: "*",
+        protocol: "http",
+      },
+      {
+        hostname: "*",
+        protocol: "https",
+      },
+    ],
+  },
+  experimental: {
+    serverComponentsExternalPackages: [
+      "@resvg/resvg-js",
+      "airtable",
+      "@olli/kvdex",
+      "@deno/kv",
+    ],
+  },
+  webpack: (config) => {
+    config.externals.push({
+      sharp: "commonjs sharp",
+      "@resvg/resvg-js": "commonjs @resvg/resvg-js",
+    });
+
+    return config;
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+
+export default withAxiom(nextConfig);
