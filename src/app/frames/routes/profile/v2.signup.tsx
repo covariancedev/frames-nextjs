bfc9c3d76d5f91d57b8ceb60c8045a88167bea1f
TheVirginBrokey <itswaptik@gmail.com>
2024-06-21 18:04:29 +0000

    v2 working

--- src/app/frames/routes/profile/v2.signup.tsx
+++ src/app/frames/routes/profile/v2.signup.tsx
@@ -198,7 +198,7 @@ app.frame("/check_user_status/:hub", async (c) => {
 					<Button action={`/apply/${hub.code}`} key="first">
 						{"🔙"}
 					</Button>,
-					<Button action={`/add_profile_data/${hub.code}/start`} key="start">
+					<Button action={`/add_profile_data/${hub.code}/email`} key="start">
 						Create Profile
 					</Button>,
 				]
@@ -231,10 +231,10 @@ app.frame("/add_profile_data/:hub/:info", async (c) => {
 	let { info } = c.req.param();
 	const { inputText, frameData, deriveState } = c;
 	const state = deriveState((previousState) => {
-		if (inputText && !["start"].includes(info)) {
+		if (inputText) {
 			const hubState = previousState.info[hub.code] as Record<string, unknown>;
 			hubState[
-				info === "launch" && hub.code !== "covariance" ? "telegram" : info
+				info === "name" ? "email" : info === "finished" ? "name" : info
 			] = inputText;
 			previousState.info[hub.code] = hubState;
 		}
@@ -254,13 +254,9 @@ app.frame("/add_profile_data/:hub/:info", async (c) => {
 		});
 	}
 	let placeholder = "";
-	let label = "";
-	let sublabel = "";
 	let next = "";
 	let previous = "";
-	let isError = false;
-	let isPartOfHub = false;
-	const userName = (hubInfo.name ?? hubInfo.launch) as string;
+	const userName = hubInfo.name as string;
 
 	console.log(`add_profile_data/${hub.code} >> info: ${info}`, {
 		hubInfo,
@@ -282,12 +278,12 @@ app.frame("/add_profile_data/:hub/:info", async (c) => {
 			existingContributorFromFid.length,
 		);
 
-		const checkError = !["launch", "start"].includes(info);
+		const checkError = !["launch", "email"].includes(info);
 		const emailExists = false;
 		let image = "";
 		console.log("checkError", { checkError, inputText });
 
-		if (info === "start") {
+		if (info === "name") {
 			if (existingContributorFromFid[0]) {
 				state.profileId = existingContributorFromFid[0].id;
 				const hubs = (existingContributorFromFid[0]?.fields.Hubs ??
@@ -297,7 +293,6 @@ app.frame("/add_profile_data/:hub/:info", async (c) => {
 					hubs,
 				);
 				state.hubs.push(...hubs);
-				isPartOfHub = !!hubs.find((h) => h === hub.id);
 				const ugid = existingContributorFromFid[0]?.fields[
 					"User Groups"
 				] as string[];
@@ -316,8 +311,8 @@ app.frame("/add_profile_data/:hub/:info", async (c) => {
 						hubInfo.name = userName ?? (user.fields.Name as string);
 						console.log(`user group for ${state.user.fid}`, user.fields.Name);
 						// return c.error({ message: 'You are already here' })
-						if (!hasTg) {
-							info = "name";
+						if (!hasTg && hub.code === "covariance") {
+							info = "telegram";
 						} else {
 							state.info[hub.code] = {
 								...(state.info[hub.code] ?? {}),
@@ -404,10 +399,6 @@ app.frame("/add_profile_data/:hub/:info", async (c) => {
 		}
 
 		if (info === "finished") {
-			console.log(
-				`add_profile_data/${hub.code} >> farcasterUrl: ` +
-					`https://warpcast.com/${state.user.username}`,
-			);
 			console.log(`add_profile_data/${hub.code} >> hubInfo: `, hubInfo);
 
 			const fcUser = await getFcUser(state.user.username);
@@ -459,15 +450,12 @@ app.frame("/add_profile_data/:hub/:info", async (c) => {
 					const userGroup = await airtable.user_group.create({
 						Name: userName as string,
 						"E-mail": hubInfo.email as string,
-						// "Farcaster": `https://warpcast.com/${state.user.username}`
 						"User Groups": [hub.name],
 					});
 					console.log(
 						`add_profile_data/${hub.code} >> created user group for ${hubInfo?.email}`,
 						userGroup.fields,
 					);
-
-					isPartOfHub = true;
 
 					state.userGroupId = userGroup.id;
 				}
@@ -478,47 +466,42 @@ app.frame("/add_profile_data/:hub/:info", async (c) => {
 			});
 
 			if (state.userGroupId) {
-				try {
-					const user = await airtable.user_group.find(state.userGroupId);
-					console.log(
-						`add_profile_data/${hub.code} >> user`,
-						user.id,
-						user?.fields,
-					);
-					if (user) {
-						const ugs = (user.fields["User Groups"] ?? []) as string[];
-						isPartOfHub = ugs.includes(hub.name);
+				const user = await airtable.user_group.find(state.userGroupId);
+				console.log(
+					`add_profile_data/${hub.code} >> user`,
+					user.id,
+					user?.fields,
+				);
+				if (user) {
+					const ugs = (user.fields["User Groups"] ?? []) as string[];
 
-						if (!ugs.includes(hub.name)) {
-							const groups = [...ugs, hub.name];
-							console.log(
-								`add_profile_data/${hub.code} >> updating user groups for ${
-									state.user.username
-								} from "${ugs.join(",")}" to "${groups.join(",")}"`,
-							);
-							const updated = await airtable.user_group.update(
-								state.userGroupId,
-								{
-									"User Groups": groups,
-								},
-							);
-							console.log(
-								`add_profile_data/${
-									hub.code
-								} >> successfully updated user groups in ug table for ${
-									state.user.username
-								} to "${(updated.fields["User Groups"] as string[]).join(
-									", ",
-								)}"`,
-							);
-						}
+					if (!ugs.includes(hub.name)) {
+						const groups = [...ugs, hub.name];
+						console.log(
+							`add_profile_data/${hub.code} >> updating user groups for ${
+								state.user.username
+							} from "${ugs.join(",")}" to "${groups.join(",")}"`,
+						);
+						const updated = await airtable.user_group.update(
+							state.userGroupId,
+							{
+								"User Groups": groups,
+							},
+						);
+						console.log(
+							`add_profile_data/${
+								hub.code
+							} >> successfully updated user groups in ug table for ${
+								state.user.username
+							} to "${(updated.fields["User Groups"] as string[]).join(", ")}"`,
+						);
 					}
-				} catch {}
+				}
 			}
 
 			if (!state.profileId) {
 				console.log(
-					`add_profile_data/${hub.code} >> Checking if user is already in the hub...`,
+					`add_profile_data/${hub.code} >> Checking if user is has a contributor profile...`,
 				);
 				const contributorByEmail = await airtable.contributors
 					.select({
@@ -541,7 +524,7 @@ app.frame("/add_profile_data/:hub/:info", async (c) => {
 					state.profileId = contributor?.id;
 				} else {
 					console.log(
-						`add_profile_data/${hub.code} >> User not found in the hub ${hub.code}...`,
+						`add_profile_data/${hub.code} >> User is not a contributor yet`,
 					);
 				}
 			}
@@ -549,66 +532,64 @@ app.frame("/add_profile_data/:hub/:info", async (c) => {
 			console.log(
 				`add_profile_data/${hub.code} >> state.profileId`,
 				state.profileId,
-				{ isPartOfHub, userGroupId: state.userGroupId },
+				{ userGroupId: state.userGroupId },
 			);
 
-			if (!isPartOfHub) {
-				let telegramUsername = hubInfo?.telegram as string | undefined;
-				// remove "@" from username
-				telegramUsername = telegramUsername
-					?.replace("@", "")
-					.trim()
-					.toLowerCase();
+			let telegramUsername = hubInfo?.telegram as string | undefined;
+			// remove "@" from username
+			telegramUsername = telegramUsername
+				?.replace("@", "")
+				.trim()
+				.toLowerCase();
 
-				if (state.profileId) {
-					const hubs = [...new Set<string>([...state.hubs, hub.id])];
-					console.log(
-						`add_profile_data/${hub.code} >> Updating existing contributor for ${farcasterUrl}`,
-						state.profileId,
-						{ hubs, userName },
-					);
-					const updated = await airtable.contributors.update(state.profileId, {
-						Hubs: hubs,
+			if (state.profileId) {
+				const hubs = [...new Set<string>([...state.hubs, hub.id])];
+				console.log(
+					`add_profile_data/${hub.code} >> Updating existing contributor for ${farcasterUrl}`,
+					state.profileId,
+					{ hubs, userName },
+				);
+				const updated = await airtable.contributors.update(state.profileId, {
+					Hubs: hubs,
+					Name: userName,
+					Farcaster: `https://warpcast.com/${state.user.username}`,
+					...(telegramUsername
+						? { Telegram: `https://t.me/${telegramUsername}` }
+						: {}),
+				});
+				console.log(
+					`add_profile_data/${hub.code} >> updated contributor profile for ${farcasterUrl}`,
+					updated.fields.Hubs,
+				);
+			} else {
+				console.log(
+					`add_profile_data/${hub.code} >> Creating new contributor for ${farcasterUrl}`,
+					{ hubInfo },
+				);
+				const profile = await airtable.contributors.create(
+					{
 						Name: userName,
+						Email: hubInfo.email as string,
+						Notes: fcUser.bio,
+						ToS: true,
+						Telegram: telegramUsername,
 						Farcaster: `https://warpcast.com/${state.user.username}`,
-						...(telegramUsername
-							? { Telegram: `https://t.me/${telegramUsername}` }
-							: {}),
-					});
-					console.log(
-						`add_profile_data/${hub.code} >> updated contributor profile for ${farcasterUrl}`,
-						updated.fields.Hubs,
-					);
-				} else {
-					console.log(
-						`add_profile_data/${hub.code} >> Creating new contributor for ${farcasterUrl}`,
-						{ hubInfo },
-					);
-					const profile = await airtable.contributors.create(
-						{
-							Name: userName,
-							Email: hubInfo.email as string,
-							Notes: fcUser.bio,
-							ToS: true,
-							Telegram: telegramUsername,
-							Farcaster: `https://warpcast.com/${state.user.username}`,
-							Source: ["Farcaster"],
-							Hubs: [hub.id],
-							"Referred by": "Lior Goldenberg",
-							"User Groups": [state.userGroupId],
-							// @ts-ignore
-							"Profile Picture": [{ url: fcUser.pfp }] as { url: string }[],
-							"Invite Code": config.inviteCode,
-						},
-						{ typecast: true },
-					);
-					// @ts-ignore
-					console.log(
-						`add_profile_data/${hub.code} >> created contributor profile for ${farcasterUrl}`,
-						profile?.id,
-					);
-					state.profileId = (profile as unknown as { id: string }).id;
-				}
+						Source: ["Farcaster"],
+						Hubs: [hub.id],
+						"Referred by": "Lior Goldenberg",
+						"User Groups": [state.userGroupId],
+						// @ts-ignore
+						"Profile Picture": [{ url: fcUser.pfp }] as { url: string }[],
+						"Invite Code": config.inviteCode,
+					},
+					{ typecast: true },
+				);
+				// @ts-ignore
+				console.log(
+					`add_profile_data/${hub.code} >> created contributor profile for ${farcasterUrl}`,
+					profile?.id,
+				);
+				state.profileId = (profile as unknown as { id: string }).id;
 			}
 
 			if (state.profileId) {
@@ -659,85 +640,40 @@ app.frame("/add_profile_data/:hub/:info", async (c) => {
 		switch (info) {
 			case "email":
 				{
-					next = hub.code === "coowncaster" ? "launch" : "name";
-					previous = "start";
-					placeholder = "John Doe";
-					image = "name";
+					next = "name";
+					placeholder = "email@example.com";
+					image = "email";
 				}
 				break;
 
 			case "name":
 				{
-					next = "finished";
+					next = "launch";
 					previous = "email";
-					label = "What's your telegram username?";
-					image = "telegram";
-					sublabel =
-						"We will use this to cross check your farcaster profile on Telegram";
-					placeholder = "durov";
+					placeholder = "John Doe";
+					image = "name";
 				}
 				break;
 
 			case "finished":
 				{
-					image = "finished";
-					next = "launch";
-				}
-				break;
-
-			case "launch":
-				{
 					image = "launch";
-				}
-				break;
-
-			default:
-				{
-					next = "email";
-					placeholder = "email@example.com";
-					image = "email";
+					// next = "launch";
 				}
 				break;
 		}
 
-		image = ["launch", "finished"].includes(info)
-			? image
-			: `${image}-${isError ? "error" : "input"}`;
+		image = ["launch", "finished"].includes(info) ? image : `${image}-input`;
 		console.log(`info: ${info}`, {
 			next,
 			previous,
 			placeholder,
-			label,
-			sublabel,
 			image,
 		});
 
 		const intents: FrameIntent[] = [];
 
-		if (info !== "launch") {
-			if (next === "launch" && hub.code === "coowncaster") {
-				intents.push(
-					<Button action={`/add_profile_data/${hub.code}/${next}`}>
-						Click to Log-in
-					</Button>,
-				);
-			} else {
-				intents.push(
-					...[
-						<TextInput placeholder={placeholder} key="text" />,
-						<Button
-							action={`/add_profile_data/${hub.code}/${previous}`}
-							key={"prev"}
-						>
-							Back
-						</Button>,
-						<Button action={`/add_profile_data/${hub.code}/${next}`} key="next">
-							Next
-						</Button>,
-					],
-				);
-			}
-		} else {
+		if (info === "launch") {
 			if (hub.code === "coowncaster") {
 				intents.push(
 					<Button.Link href={hub.groupChat}>Join Group Chat</Button.Link>,
